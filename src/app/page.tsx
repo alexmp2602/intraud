3dc56449b81f2e73f673cbfd3098d1e19c6230fe
nicolaseg2001 <nicolaseg@live.com.ar>
2026-09-02 15:34:49 -0300

    añadimos catalogo al home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import CompanyIntro from "@/components/home/CompanyIntro";
 import FinalCta from "@/components/home/FinalCta";
 import Hero from "@/components/home/Hero";
 import ProductFamilies from "@/components/home/ProductFamilies";
+import Catalogo from "@/components/home/Catalogo";
 import TechnicalService from "@/components/home/TechnicalService";
+
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
       <Hero />
       <ProductFamilies />
       <CompanyIntro />
+      <Catalogo />
       <TechnicalService />
       <FinalCta />
     </main>
